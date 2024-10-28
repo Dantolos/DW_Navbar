4fb32fb424cfce148d28a6177be8175db4c83429
Dantolos <45291569+Dantolos@users.noreply.github.com>
2024-10-28 15:04:54 +0100

    1.0.91

--- dw-navbar.php
+++ dw-navbar.php
@@ -8,7 +8,7 @@
  * Plugin Name:       DW Navbar
  * Plugin URI:        https://github.com/Dantolos/DW_Navbar/
  * Description:       Wordpress Plugin to integrate global navigation bar from demenzworld.com
- * Version:           1.0.8
+ * Version:           1.0.91
  * Author:            Aaron
  * Author URI:        https://github.com/Dantolos/
  * License:           GPL-2.0+
@@ -36,12 +36,12 @@ function load_navbar_from_api() {
      $active = get_option('dw_navigation_activate');
      if( !$active ){ return; }
 
-     $api_url = get_option('dw_navigation_api_url') ?: 'http://localhost:10038/wp-json/dw/navbar';
+     $api_url = get_option('dw_navigation_api_url') ?: 'https://demenzworld.com/wp-json/dw/navbar';
      $response = wp_remote_get($api_url, array( 'sslverify' => false ));
  
      // escape, if api doesn't work
      if (is_wp_error($response)) { 
-          error_log('Error fetching navbar: ' . $response->get_error_message());
+          var_dump('Error fetching navbar: ' . $response->get_error_message());
           return;
      }
   
